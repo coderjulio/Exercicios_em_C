b38a2bb1af960accd1bc028ac5c4090d48597cb9
Júlio César Senestro <120727933+coderjulio@users.noreply.github.com>
2023-10-04 10:52:32 -0300

    Create 3.c

---
+++ Aulas_faculdade/3.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, char const *argv[])
+{
+
+    // int n1, n2, opr;
+    // double a, s, d ,m;
+
+    // printf("Insira um numero: \n");
+    // scanf("%d", &n1);
+    // printf("Insira outro numero: \n");
+    // scanf("%d", &n2);
+    // printf("Insira uma operação: \n Somar-1 \n Subtrair-2 \n Dividir-3 \n Mltiplicar-4 \n");
+    // scanf("%d", &opr);
+
+    // a = n1 + n2;
+    // s = n1 - n2;
+    // d = n1 / n2;
+    // m = n1 * n2;
+
+    // switch(opr) {
+    //     case 1:
+    //     printf("Resultado: %.2lf", a);
+    //     case 2:
+    //     printf("Resultado: %.2lf", s);
+    //     case 3:
+    //     if(n2 == 0) {
+    //         printf("Não da pra dividir por 0 \n");
+    //     }
+    //     printf("Resultado: %.2lf", d);
+    //     case 4:
+    //     printf("Resultado: %.2lf", m);
+    // }
+
+    // int mes;
+
+    // printf("Digite um numero de 1 a 12 de acordo com o mes: \n");
+    // scanf("%d", &mes);
+
+    // switch(mes) {
+    //     case 1:
+    //         printf("Janeiro \n");
+    //         break;
+    //     case 2:
+    //         printf("Fevereiro \n");
+    //         break;
+    //     case 3:
+    //         printf("Marco \n");
+    //         break;
+    //     case 4:
+    //         printf("Abril \n");
+    //         break;
+    //     case 5:
+    //         printf("Maio \n");
+    //         break;
+    //     case 6:
+    //         printf("Junho \n");
+    //         break;
+    //     case 7:
+    //         printf("Julho \n");
+    //         break;
+    //     case 8:
+    //         printf("Agosto \n");
+    //         break;
+    //     case 9:
+    //         printf("Setembro \n");
+    //         break;
+    //     case 10:
+    //         printf("Outubro\n");
+    //         break;
+    //     case 11:
+    //         printf("Novembro\n");
+    //         break;
+    //     case 12:
+    //         printf("Dezembro \n");
+    //         break;
+    // }
+
+    
+
+
+    return 0;
+
+}
